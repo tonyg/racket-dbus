31ae1d32b84f2bfef9cd723a8187b980753d90d1
Tony Garnock-Jones <tonygarnockjones@gmail.com>
2016-09-18 07:14:02 -0400

    Very simple program exercising the library

---
+++ dbus/examples/hello.rkt
@@ -0,0 +1,25 @@
+#lang racket/base
+
+(require racket/class
+         racket/port
+         racket/pretty
+         dbus
+         dbus/interface
+         xml)
+
+(parameterize ((current-dbus-connection (dbus-connect-session-bus)))
+  (define m (dbus-manager))
+  (define unique-name (send m Hello)) ;; the connection's "unique name"
+  ;; (pretty-print
+  ;;  (with-input-from-string (send m Introspect) (compose xml->xexpr
+  ;;                                                       (eliminate-whitespace '() not)
+  ;;                                                       document-element
+  ;;                                                       read-xml)))
+  ;; (pretty-print `((children ,(dbus-introspect-children m))
+  ;;                 (methods ,(dbus-introspect-methods m))
+  ;;                 (signals ,(dbus-introspect-signals m))))
+  (pretty-print `((names ,(send m ListNames))
+                  (activatable-names ,(send m ListActivatableNames))))
+  (dbus-listen
+   (lambda args
+     (printf "Asynchronous event: ~v\n" args))))
