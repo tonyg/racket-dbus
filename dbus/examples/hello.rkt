3fd7eb7eb4b1c3b014c0daa5cc3f5635e72a1f93
Tony Garnock-Jones <tonygarnockjones@gmail.com>
2016-09-18 18:07:13 -0400

    Move example program into module+ main, for autobuilder convenience

--- dbus/examples/hello.rkt
+++ dbus/examples/hello.rkt
@@ -1,25 +1,26 @@
 #lang racket/base
 
-(require racket/class
-         racket/port
-         racket/pretty
-         dbus
-         dbus/interface
-         xml)
+(module+ main
+  (require racket/class
+           racket/port
+           racket/pretty
+           dbus
+           dbus/interface
+           xml)
 
-(parameterize ((current-dbus-connection (dbus-connect-session-bus)))
-  (define m (dbus-manager))
-  (define unique-name (send m Hello)) ;; the connection's "unique name"
-  ;; (pretty-print
-  ;;  (with-input-from-string (send m Introspect) (compose xml->xexpr
-  ;;                                                       (eliminate-whitespace '() not)
-  ;;                                                       document-element
-  ;;                                                       read-xml)))
-  ;; (pretty-print `((children ,(dbus-introspect-children m))
-  ;;                 (methods ,(dbus-introspect-methods m))
-  ;;                 (signals ,(dbus-introspect-signals m))))
-  (pretty-print `((names ,(send m ListNames))
-                  (activatable-names ,(send m ListActivatableNames))))
-  (let loop ()
-    (printf "Asynchronous event: ~v\n" (sync (dbus-listen-evt)))
-    (loop)))
+  (parameterize ((current-dbus-connection (dbus-connect-session-bus)))
+    (define m (dbus-manager))
+    (define unique-name (send m Hello)) ;; the connection's "unique name"
+    ;; (pretty-print
+    ;;  (with-input-from-string (send m Introspect) (compose xml->xexpr
+    ;;                                                       (eliminate-whitespace '() not)
+    ;;                                                       document-element
+    ;;                                                       read-xml)))
+    ;; (pretty-print `((children ,(dbus-introspect-children m))
+    ;;                 (methods ,(dbus-introspect-methods m))
+    ;;                 (signals ,(dbus-introspect-signals m))))
+    (pretty-print `((names ,(send m ListNames))
+                    (activatable-names ,(send m ListActivatableNames))))
+    (let loop ()
+      (printf "Asynchronous event: ~v\n" (sync (dbus-listen-evt)))
+      (loop))))
