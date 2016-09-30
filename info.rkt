82b598d8e0c97b5983740a033163e227588181df
Tony Garnock-Jones <tonygarnockjones@gmail.com>
2016-09-30 12:51:43 -0400

    Fix deps

--- info.rkt
+++ info.rkt
@@ -3,6 +3,11 @@
 (define collection 'multi)
 (define deps '("xexpr-path"
                "misc1" ;; for misc1/throw
+               "base"
+               "parser-tools-lib"
+               "unstable-lib"
                ))
+(define build-deps '("racket-doc"
+                     "scribble-lib"))
 
 ; vim:set ts=2 sw=2 et:
